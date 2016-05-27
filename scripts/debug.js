42a8959f1cf4ad43283664ae5ef8091a7da488ac
twahyuni <ti.wahyuni@gmail.com>
2016-05-27 13:50:07 +0800

    indentation fix

--- scripts/debug.js
+++ scripts/debug.js
@@ -1,50 +1,48 @@
 var app = app || {};
 
-          app.func9 = function(){
-            console.log("inside function9")
-            console.log("The end");
-          }
+app.func9 = function(){
+  console.log("inside function9")
+  console.log("The end");
+}
 
-app.func8 = function()
-{
+app.func8 = function(){
   console.log("inside function8")
-  setTimeout(function()
-  {
+  setTimeout(function(){
     app.func9()
-  },
-  0);
+  }, 0);
 }
 
 app.func7 = function(){
-  console.log("inside function7")
-  setTimeout(function(){ app.func8() }, 1000);
-}
-
-app.func5 = function(){
-console.log("inside function5");
-app.func6()
-}
-
-app.func6 = function(){
-      console.log("inside function6")
+  log("inside function7")
   setTimeout(function(){
-    app.func7()
-}, 1000, 100);
-}
-
-
-
-  app.func3 = function(){
-    console.log("inside function3");
-    app.func4()
-  }
-
-app.func4 = function(){ console.log("inside function4");
-  setTimeout(function(){ app.func5()
+    app.func8()
   }, 1000);
 }
 
+app.func5 = function(){
+  console.log("inside function5");
+  app.func6()
+}
 
+app.func6 = function(){
+  console.log("inside function6")
+  setTimeout(function(){
+    app.func7()
+  }, 1000, 100);
+}
+
+
+app.func3 = function(){
+  console.log("inside function3");
+  app.func4()
+}
+
+app.func4 = function(){
+  console.log("inside function4");
+  setTimeout(function(){
+    app.func5()
+  }, 1000);
+}
 
 app.func2 = function(){
   console.log ("inside function2");
@@ -52,11 +50,9 @@ app.func2 = function(){
 }
 
 app.func1 = function(){
-        console.log("inside function1");
-        app.func2()
-      }
-
-
+  console.log("inside function1");
+  app.func2()
+}
 
 
 app.initialize = function(){
